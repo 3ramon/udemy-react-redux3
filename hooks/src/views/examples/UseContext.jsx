9d32fb949dde397265563fac598460a30cc8af2a
3ramon <ramon.asilva@bandtec.com.br>
2024-10-30 23:55:42 -0300

    useREf, useMemo, useCallback e contextAPI com useContext

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useContext } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import DataContext from "../../data/DataContext";
 
 const UseContext = (props) => {
+    const context = useContext(DataContext);
+
+    function setNumber(delta) {
+        context.setState({
+            ...context.state,
+            number: context.state.number + delta,
+        });
+    }
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
-        </div>
-    )
-}
 
-export default UseContext
+            <div className="center">
+                <span className="text">{context.state.text}</span>
+                <span className="text">{context.state.number}</span>
+                <div>
+                    <button className="btn" onClick={() => setNumber(+1)}>
+                        +1
+                    </button>
+                    <button className="btn" onClick={() => setNumber(-1)}>
+                        -1
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UseContext;
